c0960d574fa0d74d8e5527a96a562cb77076606a
Rajjat Layal <amanlayalupwork@gmail.com>
2024-07-31 15:12:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST","GET"],
+  credentials: true
+}));
 
 const port = process.env.PORT || 5000;
 const baseURL = process.env.MONGO_URL;
